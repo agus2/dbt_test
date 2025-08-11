073ff88196b4c945d4256512f1e331dda030d9e7
agus_pc <abzyou001@gmail.com>
2025-08-11 11:29:37 +0200

    Limpieza: rama vacía a partir de main

--- macros/audit_columns.sql
+++
@@ -1,4 +0,0 @@
-{% macro audit_columns() %}
-    '{{ var("job_run_id") }}'::string as job_run_id,
-    '{{ var("job_run_date") }}'::timestamp as job_run_date
-{% endmacro %}
--- models/dim/dim_customers.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-
-select distinct
-    * EXCLUDE (job_run_id, job_run_date),
-    '{{ invocation_id }}' as log_id, -- ID de ejecución DBT
-    current_timestamp() as execution_date
-from {{ ref("stg_CUSTOMER") }} c
